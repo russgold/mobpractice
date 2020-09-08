75804f7082b30595090060dfb8ddc573941ea48d
Russell Gold <russell.gold@oracle.com>
2020-09-08 17:11:54 -0400

    more

--- src/main/java/mobbing/TicTacToe.java
+++ src/main/java/mobbing/TicTacToe.java
@@ -1,7 +1,25 @@
 package mobbing;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class TicTacToe {
+  private Player currentPlayer = Player.X;
+  private Player[] board = new Player[9];
+
   public boolean isEmpty() {
-    return true;
+    return Arrays.stream(board).allMatch(Objects::isNull);
+  }
+
+  public Player currentPlayer() {
+    return currentPlayer;
+  }
+
+  public void place(int i) {
+    board[i] = currentPlayer;
+  }
+
+  public Player getMove(int i) {
+    return board[i];
   }
 }
--- src/test/java/mobbing/TicTacToeTest.java
+++ src/test/java/mobbing/TicTacToeTest.java
@@ -2,6 +2,9 @@ package mobbing;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 public class TicTacToeTest {
@@ -13,7 +16,21 @@ public class TicTacToeTest {
   }
 
   @Test
-  public void afterFirstMove_boardIsNotEmpty() {
+  public void whenBoardCreated_currentPlayerIsX() {
     assertEquals(game.currentPlayer(), Player.X);
+  }
+
+  @Test
+  public void whenMoveMade_boardIsNotEmpty() {
+    game.place(1);
+
+    assertFalse(game.isEmpty());
+  }
+
+  @Test
+  public void whenMoveMade_cellIsSet() {
+    game.place(1);
+
+    assertSame(game.getMove(1), Player.X);
   }
 }
