6983a6e88dfafb54295333797b3e4b745f3d47e1
Dongbo Xiao <dongbo.xiao@oracle.com>
2020-09-09 14:16:15 -0400

    one more test

--- src/main/java/mobbing/TicTacToe.java
+++ src/main/java/mobbing/TicTacToe.java
@@ -30,4 +30,21 @@ public class TicTacToe {
   public int totalMarks() {
     return (int) Arrays.stream(board).filter(Objects::nonNull).count();
   }
+
+  public Player getWinner() {
+    if (getWinner(0,1,2) != null) return currentPlayer;
+    if (getWinner(3,4,5) != null) return currentPlayer;
+    if (getWinner(6,7,8) != null) return currentPlayer;
+    if (getWinner(0,3,6) != null) return currentPlayer;
+    if (getWinner(1,4,7) != null) return currentPlayer;
+    if (getWinner(2,5,8) != null) return currentPlayer;
+    if (getWinner(0,4,8) != null) return currentPlayer;
+    if (getWinner(2,4,6) != null) return currentPlayer;
+    return null;
+  }
+
+  private Player getWinner(int x, int y, int z) {
+    if (board[x] == currentPlayer && board[y] == currentPlayer && board[z] == currentPlayer) return currentPlayer;
+    return null;
+  }
 }
